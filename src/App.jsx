ee9dd1872a49783dd0e38b172e6f1e8b07e34730
RyanGiggs2205 <163643629+RyanGiggs2205@users.noreply.github.com>
2025-02-27 07:40:55 +0000

    App2.0

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ const App = () => {
   const [books, setBooks] = useState([]);
   const [newBook, setNewBook] = useState({ title: '', author: '', image_url: ''});
   const [editBook, setEditBook] = useState(null);
-  const uri = 'https://scaling-parakeet-5gv9p6vpv6c4977-5001.app.github.dev/'
+  const uri = 'https://super-duper-giggle-wrv54w76g4gw39w49-5001.app.github.dev/'
   useEffect(() => {
     fetchBooks();
   }, []);
